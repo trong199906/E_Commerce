5431c2c35e15573ffe9e2cdb6516dcaf801534cf
TrongND <interns@arius.com.vn>
2021-03-15 17:07:30 +0700

    add a update item

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('Cart', views.cart, name='cart'),
-    path('Checkout', views.checkout, name='checkout')
+    path('Checkout', views.checkout, name='checkout'),
+    path('UpdateItem/', views.updated_item, name='updateitem')
 ]
