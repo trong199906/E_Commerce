b500c4cfe8540472a3948633e459d9e096e69fc7
TrongND <interns@arius.com.vn>
2021-03-15 16:18:00 +0700

    update a models and process order , orderitem

--- store/models.py
+++ store/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
             url = ''
         return url
 
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
