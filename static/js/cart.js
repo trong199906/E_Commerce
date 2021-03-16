7d3b3a11cc0069094502556cb582a3aebb16a40b
TrongND <interns@arius.com.vn>
2021-03-16 13:14:20 +0700

    update for a cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,16 +1,38 @@
-var updateBtns = document.getElementsByClassName("update-cart")
-for(var i = 0; i <= updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function (){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'action:', action)
+let updateBtns = document.getElementsByClassName("update-cart")
+for(let i = 0; i <= updateBtns.length; i++) {
+        updateBtns[i].addEventListener('click', function () {
+            let productId = this.dataset.product
+            let action = this.dataset.action
+            console.log('productId:', productId, 'action:', action)
 
-        console.log('USER:', user)
-        if (user == 'AnonymousUser'){
-            console.log("User is not authenticate")
-        }
-        else {
-            console.log("user is authenticate, sending data ...")
-        }
-    })
+            console.log('USER:', user)
+            if (user == 'AnonymousUser') {
+                console.log("User is not authenticate")
+            } else {
+                updateUserOrder(productId, action)
+            }
+        })
+
 }
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+                'X-CSRFToken': csrftoken,
+			},
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    console.log('Data:', data)
+		});
+}
+
+
