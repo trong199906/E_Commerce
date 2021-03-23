e9a3fe11d61c12f3dea105badbf7e702ec66a96c
TrongND <interns@arius.com.vn>
2021-03-23 17:43:21 +0700

    update a process order for view

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Product, Customer, Order, OrderItem
+from .models import Product, Customer, Order, OrderItem, ShippingAddress
 from django.http import JsonResponse
+import datetime
 import json
 
 
@@ -75,5 +76,29 @@ def updated_item(request):
 
 
 def ProcessOrder(request):
-    print('Data:', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == order.get_card_total:
+            order.complete == True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+
+    else:
+        print('User is not logged in ..')
     return JsonResponse('Payment complete', safe=False)
