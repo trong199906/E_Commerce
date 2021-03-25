fdc5d910f6c77474c033c3a3859f9425c8045dcc
TrongND <interns@arius.com.vn>
2021-03-25 17:11:43 +0700

    update a cart

--- store/views.py
+++ store/views.py
@@ -29,9 +29,35 @@ def cart(request):
         items = order.orderitem_set.all()
         CartItems = order.get_card_item
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('Cart:', cart)
         items = []
         order = {'get_card_total': 0, 'get_card_item': 0}
         CartItems = order['get_card_item']
+
+        for i in cart:
+            CartItems += cart[i]['quantity']
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+
+            order['get_card_total'] += total
+            order['get_card_item'] += cart[i]['quantity']
+
+            item = {
+                'product': {
+                    'id': product.id,
+                    'name': product.name,
+                    'price': product.price,
+                    'imageURL': product.imageURL
+                },
+                'quantity': cart[i]['quantity'],
+                'get_total': total
+            }
+            items.append(item)
 
     context = {'items': items, 'order': order, 'CartItems': CartItems}
     return render(request, 'store/Cart.html', context)
@@ -76,6 +102,8 @@ def updated_item(request):
 
 
 from django.views.decorators.csrf import csrf_exempt
+
+
 @csrf_exempt
 def ProcessOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
