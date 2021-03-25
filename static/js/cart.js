df9f011dd2e70c7ef25c6d860bd663b0ef15e672
TrongND <interns@arius.com.vn>
2021-03-25 17:11:27 +0700

    update a add cookie

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,13 +7,39 @@ for(let i = 0; i <= updateBtns.length; i++) {
 
             console.log('USER:', user)
             if (user == 'AnonymousUser') {
-                console.log("User is not authenticate")
+                addCookieItem(productId, action)
             } else {
                 updateUserOrder(productId, action)
             }
         })
 
 }
+
+function addCookieItem(productId, action){
+	console.log("User is not authenticate")
+
+	if(action == 'add'){
+		if(cart[productId] == undefined){
+			cart[productId] = {'quantity': 1}
+		}
+		else {
+			cart[productId]['quantity'] += 1
+		}
+	}
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if(cart[productId]['quantity'] <= 0){
+			console.log('remove item')
+			delete cart[productId]
+		}
+
+	}
+	console.log('Cart:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
+}
+
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
 
